a2a21b7413b303e64f93558d2e42c41bea3c6343
stedbrown <stefanovananti@icloud.com>
2025-04-02 23:33:51 +0200

    Aggiunta sitemap per SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteUrl: 'https://mtre.ch',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  alternateRefs: [
+    {
+      href: 'https://mtre.ch/en',
+      hreflang: 'en',
+    },
+    {
+      href: 'https://mtre.ch/de',
+      hreflang: 'de',
+    },
+    {
+      href: 'https://mtre.ch/fr',
+      hreflang: 'fr',
+    },
+    {
+      href: 'https://mtre.ch',
+      hreflang: 'it',
+    },
+  ],
+} 
